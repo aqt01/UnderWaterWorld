431c64c0f7dcb7eb736e15b881e0d445628b40fe
root <root@IA2.(none)>
2013-10-07 18:14:20 -0400

    comentando game.py

--- game.py
+++ game.py
@@ -11,20 +11,24 @@ Fisheslist = []
 
 class Game:
 
-	def __init__(self, Shark_n, Fishes_n,w,h):
+	def __init__(self, Shark_n, Fishes_n,w,h): # w = 800, h = 600
+		#Se inicializa pygame
 		self.game = pygame
 		self.game.init()
+		# Se carga el path del background
 		self.background_path ="./Images/background.jpg"
 		self.Width = w
 		self.Heigth = h		
-		self.screen=pygame.display.set_mode( (800,600),0,32)
+		# Se carga el fondo con w y h que son introducidas por parametros
+		self.screen=pygame.display.set_mode( (w,h),0,32)
+		# Se carga la imagen de fondo
 		self.background = self.game.image.load(self.background_path).convert()
-		
+		# Se asigna la velocidad que seran los px que los sprites caminaran por cada paso
 		self.vel = 8
-
+		#se cargan las imagenes de tiburones y peces
 		self.Sharks_img()
 		self.Fishes_img()
-
+		
 		self.img_pos = 0
 		self.alpha = 128
 		self.Sharks_spri = pygame.sprite.Group()
@@ -33,17 +37,14 @@ class Game:
 		self.Collect_sprites()
 
 		
-	# se carga el lote de imagenes
-
-
-
-	
-
+	# La funcion se encarga de ir colectando las imagenes en dos listas mientras estas se van moviendo durante la ejecucion
 	def Collect_sprites(self):
+		# Se vacean las imagenes ya cargadas para ir cargando las demas
 
 		self.Sharks_spri.empty()
 		self.Fishes_spri.empty()
 		otherlistshark=[]
+
 		for i in range(self.Shark_n):	
 			if Sharkslist[i].alive:
 				otherlistshark.append(Sharkslist[i])
@@ -66,10 +67,10 @@ class Game:
 		
 
 
-
+	# Funcion para crear las unidades, tanto los peces como los tiburones
 
 	def Create_units(self,Shark_n,Fishes_n,vel):
-		self.pos = []
+		self.pos = [] 
 		self.vel = vel
 		self.Shark_n = Shark_n
 		self.Fishes_n = Fishes_n
@@ -115,6 +116,7 @@ class Game:
 			Fisheslist[i].start()
 
 	#Imagenes de los peces
+	# Se cargan las imagenes de los peces, el mismo proceso se repite con los tiburones
 	def Fishes_img(self):
 		
 		self.Fishes_path = "./Images/sprites/peces/"
@@ -249,19 +251,17 @@ class Game:
 	
 
 def main():
-	juego = Game(10,40,800,600) #Se recibe 1er parametro la cantidad de tiburones y 2do cantidad de peces
+	juego = Game(10,40,800,600) #Se recibe 1er parametro la cantidad de tiburones y 2do cantidad de peces,
+				   # 3er y 4to parametro son anchura y altura
 
-	while True:
-#    for event in pygame.event.get():
-#    if event.type == QUIT:
- #       pygame.quit()
-  #      sys.exit()
+	while True: # Loop, el juego se ejecuta dentro de esta clausura
 
 		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+			if event.type == pygame.QUIT: # Para salir del juego
 				exit_game()
-		juego.draw()
-		colisionDict =pygame.sprite.groupcollide(juego.Sharks_spri,juego.Fishes_spri, 0, 0)
+		juego.draw()# Para dibujjar los sprites del juego
+		colisionDict =pygame.sprite.groupcollide(juego.Sharks_spri,juego.Fishes_spri, 0, 0) # Para capturar las colisiones entre
+										# Tiburones y peces, devuelve un diccionario con colisiones
 		if  colisionDict:
 			colisiones=colisionDict.values()
 			for i in colisiones:
@@ -269,26 +269,7 @@ def main():
 					j.Die()
 				#print i
 			print "Pez Choco Con tiburon"
-		
-
-		# colisionDicts =pygame.sprite.groupcollide(juego.Fishes_spri,juego.Sharks_spri, 0, 0)
-		# if  colisionDicts:
-		# 	colisioness=colisionDicts.keys()
-		# 	#print colisiones
-		# 	for i in colisioness:
-		# 			i.Die()
-		# 		#print i
-		# 	print "Alguien Choco"
-			#print colisionDictoc
-
-		
-
-
-
-		
 		pygame.display.update()
-
-    #	pygame.display.update()
 
 if __name__ == "__main__":
 	main()
